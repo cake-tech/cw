671838caff822276add2c793183a5d196a6878da
Mykola Misiura <nicolas.misiura@gmail.com>
2019-07-20 18:01:38 +0300

    Fixes for build zeromq and lmdb

--- install_lmdb.sh
+++ install_lmdb.sh
@@ -4,7 +4,7 @@ LMDB_DIR_PATH="$SOURCE_DIR/lmdb/Sources"
 
 echo "============================ LMDB ============================"
 echo "Cloning lmdb from - $LMDB_DIR_URL"
-git clone $LMDB_DIR_URL $LMDB_DIR_PATH
+git clone -b build $LMDB_DIR_URL $LMDB_DIR_PATH
 cd $LMDB_DIR_PATH
 git checkout b9495245b4b96ad6698849e1c1c816c346f27c3c
 cd $SOURCE_DIR
