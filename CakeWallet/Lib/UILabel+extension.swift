7e4c424b0a6fc575893ff60f595c83029d9fb32b
Tanner Silva <t@tannersilva.com>
2019-09-07 18:28:06 +0000

    fixed bug: UILabel was using background text color

--- CakeWallet/Lib/UILabel+extension.swift
+++ CakeWallet/Lib/UILabel+extension.swift
@@ -5,33 +5,33 @@ extension UILabel {
         self.init()
         self.font = font
         self.numberOfLines = 0
-        self.textColor = UserInterfaceTheme.current.background
+        self.textColor = UserInterfaceTheme.current.text
     }
     
     convenience init(fontSize size: CGFloat = 16) {
         self.init()
         self.font = applyFont(ofSize: size)
-        self.textColor = UserInterfaceTheme.current.background
+        self.textColor = UserInterfaceTheme.current.text
     }
     
     convenience init(text: String = "") {
         self.init()
         self.text = text
-        self.textColor = UserInterfaceTheme.current.background
+        self.textColor = UserInterfaceTheme.current.text
     }
     
     static func withLightText(font: UIFont = UIFont.systemFont(ofSize: 16)) -> UILabel {
         let label = UILabel(font: font)
         
         //tstag
-        label.textColor = UserInterfaceTheme.current.background
+        label.textColor = UserInterfaceTheme.current.text
         label.numberOfLines = 0
         return label
     }
     
     static func withLightText(fontSize size: CGFloat = 16) -> UILabel {
         let label = UILabel(fontSize: size)
-        label.textColor = UserInterfaceTheme.current.background
+        label.textColor = UserInterfaceTheme.current.text
         label.font = applyFont(ofSize: size)
         label.numberOfLines = 0
         return label
