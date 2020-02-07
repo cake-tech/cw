bd8a24e496dd073a915ce049b13e9ecf5e09e7e5
Tanner Silva <t@tannersilva.com>
2020-02-07 01:49:35 -0600

    added the openalias resolver for XMR addresses

--- CakeWallet/Lib/DNS/OpenAliasResolver.swift
+++ CakeWallet/Lib/DNS/OpenAliasResolver.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import Socket
+import SSLService
+
+fileprivate let oaQueue = DispatchQueue(label: "com.cakewallet.openaliasResolver", attributes:[.concurrent])
+
+public class OpenAlias {
+    public enum ResolutionError: Error {
+        case unexpectedResponse
+        case queryTimeout
+        case invalidTimeout
+    }
+    
+    private static let defaultSSLConfig = SSLService.Configuration.init(withCipherSuite: "ALL", clientAllowsSelfSignedCertificates: false, embeddedServerCertPaths: nil)
+    
+    private class func getSerializedTCPDNSRequest(query inputQueryName:String) throws -> Data {
+        var newRequest = Message(type:.query, questions:[Question(name:inputQueryName, type:.text)])
+        newRequest.recursionAvailable = true
+        newRequest.recursionDesired = true
+        return try newRequest.serializeTCP()
+    }
+    
+    private class func resolveOverTLS(request:Data, endpoint:String="1.1.1.1", port:Int32=853, timeoutSeconds:TimeInterval) throws -> String {
+        guard timeoutSeconds > 0 else {
+            throw OpenAlias.ResolutionError.invalidTimeout
+        }
+        
+        let socket = try Socket.create(family:.inet, type:Socket.SocketType.stream, proto:Socket.SocketProtocol.tcp)
+        socket.delegate = try SSLService(usingConfiguration:SSLService.Configuration.init(withCipherSuite: "ALL", clientAllowsSelfSignedCertificates: false, embeddedServerCertPaths: nil))
+        try socket.connect(to:endpoint, port:port, timeout:UInt(timeoutSeconds*1000))
+        var readData = Data()
+        try socket.write(from:request)
+        try socket.setReadTimeout(value: UInt(timeoutSeconds*1000))
+        repeat {
+            do {
+                usleep(100000)
+                try socket.read(into:&readData)
+            } catch _ {
+                throw OpenAlias.ResolutionError.queryTimeout
+            }
+        } while (readData.count == 0)
+        let response = try Message.init(deserializeTCP: readData)
+        if let gotResponse = response.answers.first, let textResponse = gotResponse as? TextRecord, let openaliasXMRAddress = textResponse.attributes["oa1:xmr recipient_address"] {
+            return openaliasXMRAddress
+        }
+        throw OpenAlias.ResolutionError.unexpectedResponse
+    }
+
+    public class func resolve(jobID:UInt64, query:String, _ success:@escaping (UInt64, String) -> Void) {
+        oaQueue.async {
+            do {
+                let requestData = try Self.getSerializedTCPDNSRequest(query: query)
+                let resolvedAddress = try Self.resolveOverTLS(request: requestData, timeoutSeconds: 4)
+                success(jobID, resolvedAddress)
+            } catch let error {
+                print("[OPENALIAS] failed to resolve job id: \(jobID)")
+                print("\(error)")
+            }
+        }
+    }
+}
